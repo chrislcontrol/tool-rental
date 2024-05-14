3b9bea235c22256f6229e1fa1fd3ae07800c989e
10724111936 <10724111936@UNS11DT099863.animaedu.intranet>
2024-05-14 19:22:58 -0300

    feat: new controller for tools

---
+++ src/main/java/tool/rental/domain/controllers/RegisterToolController.java
@@ -0,0 +1,26 @@
+package tool.rental.domain.controllers;
+
+import tool.rental.domain.use_cases.RegisterToolUseCase;
+import tool.rental.utils.Controller;
+import tool.rental.utils.PresentationFrame;
+import tool.rental.utils.ToastError;
+
+import javax.swing.*;
+
+public class RegisterToolController {
+
+    public final RegisterToolController registerToolController = new RegisterToolController();
+
+    public registerToolController(PresentationFrame frame);
+    super(frame);
+   
+}
+
+public void registerTool(String name,String branch, double cost) throws ToastError {
+        this.registerToolUseCase.execute(name, branch, cost);
+
+        JOptionPane.showMessageDialog(null, "Ferramenta cadastrada com sucesso!");
+
+        this.frame.setVisible(false);
+    }
+}
