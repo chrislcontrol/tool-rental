6cd47bb5dec9e43f2636a4189b5bfe9bcd2be43b
Nicholas da Silva <125492853+Shoto29@users.noreply.github.com>
2024-05-14 21:11:26 -0300

    feat: action button 'exit' works

--- src/main/java/tool/rental/presentation/FriendsScreenFrame.java
+++ src/main/java/tool/rental/presentation/FriendsScreenFrame.java
@@ -4,10 +4,11 @@ import tool.rental.domain.controllers.AppMainController;
 import tool.rental.utils.PresentationFrame;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FriendsScreenFrame extends PresentationFrame {
-    private final AppMainController controller = new AppMainController(this);
-    private JPanel mainPanel;
     private JTable friendsTable;
     private JScrollPane JScrollPanel;
     private JPanel Panel1;
@@ -16,9 +17,33 @@ public class FriendsScreenFrame extends PresentationFrame {
     private JButton registerFriendButton;
     private JButton exitButton;
     private JButton rankingButton;
+    private JPanel MainPanel;
+
 
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+    }
+
+    public FriendsScreenFrame() {
+        this.setupPageLayout();
+        this.setMainPanel();
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    private void setupPageLayout() {
+        this.setTitle("Amigos");
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.setSize(this.userScreen.widthFraction(20), this.userScreen.heightFraction(40));
+        this.setLocationRelativeTo(null);
+    }
+
+    private void setMainPanel() {
+        this.setContentPane(this.MainPanel);
     }
 }
